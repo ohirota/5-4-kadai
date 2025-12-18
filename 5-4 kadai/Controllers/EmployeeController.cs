e48e32cd94471ada9796ddf5b4ed3c8ecf735446
ohirota <ohirota2220@gmail.com>
2025-12-18 17:57:53 +0900

    プロジェクト ファイルを追加します。

---
+++ 5-4 kadai/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using benkyou5_4.WebAPI.Data;     // DbContext の場所
+using benkyou5_4.Models;          // Employee.cs の namespace に合わせる
+
+namespace benkyou5_4.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetEmployees()
+        {
+            return Ok(_context.Employees.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+            return Ok(employee);
+        }
+    }
+}
+
+
+
---
+++ 5-4 kadai/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using benkyou5_4.Models;   // ← Employee.cs の namespace に合わせる
+
+namespace benkyou5_4.WebAPI.Data   // ← WebAPI プロジェクトの namespace に合わせる
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
+
+
+
